044acec269b9be859311510a23f00352859c6ff8
alex <alexanderdai@gmail.com>
2017-04-30 21:22:37 -0700

    added speedometer

---
+++ Assets/Scripts/SpeedometerNeedle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedometerNeedle : MonoBehaviour {
+
+    public Rigidbody car;
+    public float maxMPH = 160;
+    public float minAngle = -110;
+    public float maxAngle = 110;
+
+    private const float MPS_TO_MPH = 2.23694f;
+	
+	// Update is called once per frame
+	void Update () {
+        float v = car.velocity.magnitude * MPS_TO_MPH;
+        float angle = Mathf.Lerp(minAngle, maxAngle, v / maxMPH);
+        Vector3 tmp = transform.localEulerAngles;
+        tmp.z = angle;
+        transform.localEulerAngles = tmp;
+	}
+}
